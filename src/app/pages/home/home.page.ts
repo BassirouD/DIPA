147fddf57a23d75d30b173b7c0e48a52f6c0633c
BassirouD <moubass.diallo@gmail.com>
2021-08-20 17:51:37 +0200

    Change login page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,57 +1,137 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
-import { AuthService } from 'src/app/services/auth.service';
+import {AuthService} from 'src/app/services/auth.service';
+import {ActionSheetController} from "@ionic/angular";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+    selector: 'app-home',
+    templateUrl: './home.page.html',
+    styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
 
-  showLocationDetail = false;
-  user:any;
-  login=localStorage.getItem('login')
-  nom:any;
-  prenom:any;
-  email:any;
-  telepohne:any;
-  constructor(private authS:AuthService,private router:Router) {}
+    showLocationDetail = false;
+    user: any;
+    login = localStorage.getItem('login')
+    nom: any;
+    prenom: any;
+    email: any;
+    telepohne: any;
 
-  ngOnInit(): void {
-    //console.log(this.login)
-    this.chechUser();
-  }
+    constructor(private authS: AuthService, private router: Router, public actionSheetController: ActionSheetController) {
+    }
 
-  check(){
-    this.chechUser();
-  }
+    ngOnInit(): void {
+        //console.log(this.login)
+        this.chechUser();
+    }
 
-
-  chechUser(){
-    this.authS.getUserDetail(this.login)
-    .subscribe(resp=>{
-      this.user=resp;
-      console.log(this.user);
-      this.nom=this.user.nom
-      this.prenom=this.user.prenom
-      this.email=this.user.email;
-      this.telepohne=this.user.telephone;
-
-    })
-  }
+    check() {
+        this.chechUser();
+    }
 
 
-  checkcode(event){
-     let periode=event.target.value;
-     localStorage.setItem('periode',periode);
-  }
+    chechUser() {
+        this.authS.getUserDetail(this.login)
+            .subscribe(resp => {
+                this.user = resp;
+                console.log(this.user);
+                this.nom = this.user.nom
+                this.prenom = this.user.prenom
+                this.email = this.user.email;
+                this.telepohne = this.user.telephone;
+
+            })
+    }
 
 
-  checkannee(event){
-      let annee=event.target.value;
-      localStorage.setItem('annee',annee);
-  }
+    checkcode(event) {
+        let periode = event.target.value;
+        localStorage.setItem('periode', periode);
+    }
+
+    async presentActionSheet(event) {
+        const actionSheet = await this.actionSheetController.create({
+            header: 'Albums',
+            cssClass: 'my-custom-class',
+            buttons: [{
+                text: 'Janvier',
+                handler: () => {
+                    console.log('Delete clicked');
+                }
+            }, {
+                text: 'Février',
+                handler: () => {
+                    console.log('Share clicked');
+                }
+            }, {
+                text: 'Mars',
+                handler: () => {
+                    console.log('Play clicked');
+                }
+            }, {
+                text: 'Avril',
+                handler: () => {
+                    console.log('Favorite clicked');
+                }
+            },
+                {
+                    text: 'Mai',
+                    handler: () => {
+                        console.log('Share clicked');
+                    }
+                },
+                {
+                text: 'Juin',
+                handler: () => {
+                    console.log('Share clicked');
+                },
+            },
+                {
+                    text: 'Juillet',
+                    handler: () => {
+                        console.log('Share clicked');
+                    }
+                }, {
+                    text: 'Aout',
+                    handler: () => {
+                        console.log('Share clicked');
+                    }
+                }, {
+                    text: 'Septembre',
+                    handler: () => {
+                        console.log('Share clicked');
+                    }
+                },{
+                    text: 'Octombre',
+                    handler: () => {
+                        console.log('Share clicked');
+                    }
+                },{
+                    text: 'Novembre',
+                    handler: () => {
+                        console.log('Share clicked');
+                    }
+                }, {
+                    text: 'Decembre',
+                    handler: () => {
+                        console.log('Share clicked');
+                    }
+                },]
+        });
+        await actionSheet.present();
+
+        const { role } = await actionSheet.onDidDismiss();
+        console.log('onDidDismiss resolved with role', role);
+    }
+
+
+
+
+checkannee(event) {
+        let annee = event.target.value;
+        localStorage.setItem('annee', annee);
+    }
 
     onScroll(ev) {
         const offset = ev.detail.scrollTop;
@@ -64,27 +144,27 @@ export class HomePage implements OnInit {
         }, 2000);
     }
 
-    gotoEG(){
+    gotoEG() {
         this.router.navigate(['/etat-global-dossier']);
     }
 
-    gotoCDossier(){
+    gotoCDossier() {
         this.router.navigate(['/estimation-import-article']);
     }
 
-    gotoTE(){
+    gotoTE() {
         this.router.navigate(['/tendance-evolution-mois']);
     }
 
-    gotoTM(){
+    gotoTM() {
         this.router.navigate(['/demandes-traitees-articles']);
     }
 
-    gotoPC(){
+    gotoPC() {
         this.router.navigate(['/performance-comparee']);
     }
 
-    gotoPT(){
+    gotoPT() {
         this.router.navigate(['/estimation-import-mois']);
     }
 
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,89 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {AlertController, LoadingController, NavController, ToastController} from "@ionic/angular";
 import {Router} from "@angular/router";
-import { AuthService } from 'src/app/services/auth.service';
+import {AuthService} from 'src/app/services/auth.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
 
-  loadData={login:'',password:''} ;
-  user:any;
-  userForm:FormGroup;
-  user1:any;
-  constructor(public loadingController:LoadingController,
-              public router:Router,
-              private toastController: ToastController,
-              private alertCtrl: AlertController,
-              public nav:NavController,
-              private formBuilder:FormBuilder,private auths:AuthService) { }
+    loadData = {login: '', password: ''};
+    user: any;
+    userForm: FormGroup;
+    user1: any;
 
-  ngOnInit() {
-    this.userForm=this.formBuilder.group({
-      login:['',Validators.required],
-      password:['',Validators.required]
-    });
-  }
+    constructor(public loadingController: LoadingController,
+                public router: Router,
+                private toastController: ToastController,
+                private alertCtrl: AlertController,
+                public nav: NavController,
+                private formBuilder: FormBuilder, private auths: AuthService) {
+    }
+
+    ngOnInit() {
+        this.userForm = this.formBuilder.group({
+            login: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
 
 
-  async login1(){
-    const loading = await this.loadingController.create({
-      message:"Please wait !!!",
-      duration:3000
-    });
-    await loading.present()
+    async login1() {
+        const loading = await this.loadingController.create({
+            message: "Please wait !!!",
+            duration: 3000
+        });
+        await loading.present()
 
-    //console.log(this.userForm.value);
-    let body = `login=${this.userForm.value.login}&password=${this.userForm.value.password}`;
+        //console.log(this.userForm.value);
+        let body = `login=${this.userForm.value.login}&password=${this.userForm.value.password}`;
 
-     this.auths.login1(body)
-     .subscribe(resp=>{
-       this.user=resp;
-       //console.log('user ::'+JSON.stringify(this.user))
-      loading.dismiss();
-      //
-      if(this.user!=null){
-        localStorage.setItem('loggedIn','true');
-        localStorage.setItem('login',this.user.login)
-        localStorage.setItem('id',this.user.ID);
-        this.presentAlert("Connexion réussie !!");
-        console.log('default '+this.user.DEFAULTPWD)
-        if(this.user.DEFAULTPWD==='0'){
-          this.router.navigate(['tabs/home']);
-         }else{
-           this.router.navigate(['change-password'])
-         }
-      }else{
-        this.presentAlert("Paramétre authentification non correct!!");
-      }
+        this.auths.login1(body)
+            .subscribe(resp => {
+                this.user = resp;
+                //console.log('user ::'+JSON.stringify(this.user))
+                loading.dismiss();
+                //
+                if (this.user != null) {
+                    localStorage.setItem('loggedIn', 'true');
+                    localStorage.setItem('login', this.user.login)
+                    localStorage.setItem('id', this.user.ID);
+                    // this.presentAlert("Connexion réussie !!");
+                    console.log('default ' + this.user.DEFAULTPWD)
+                    if (this.user.DEFAULTPWD === '0') {
+                        this.router.navigate(['tabs/home']);
+                    } else {
+                        this.router.navigate(['change-password'])
+                    }
+                } else {
+                    this.presentAlert("Paramétre authentification non correct!!");
+                }
 
-     },err=>{
-       this.presentAlert("Erreur authentification!!");
-     })
+            }, err => {
+                this.presentAlert("Erreur authentification!!");
+            })
 
-  }
+    }
 
-  async presentAlert(mgs) {
-    const alert = await this.alertCtrl.create({
-      header: 'Alert',
-      message: mgs,
-      buttons: ['OK']
-    });
+    async presentAlert(mgs) {
+        const alert = await this.alertCtrl.create({
+            header: 'Alert',
+            message: mgs,
+            buttons: ['OK']
+        });
 
-    await alert.present();
+        await alert.present();
 
-  }
+    }
 
-  async presentToast(msg) {
-    const toast = await this.toastController.create({
-      message: msg,
-      duration: 2000
-    });
-    toast.present();
-  }
+    async presentToast(msg) {
+        const toast = await this.toastController.create({
+            message: msg,
+            duration: 2000
+        });
+        toast.present();
+    }
 
 }
